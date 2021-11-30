ac076dd51759f74579f86f7b0a2775daa91bd188
Hrishabh <kumarhrishabh9429@gmail.com>
2021-12-01 00:00:07 +0530

    day 4 first

--- lib/Pages/loginpage.dart
+++ lib/Pages/loginpage.dart
@@ -7,10 +7,50 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(title: Text('Login Page'),),
+      // appBar: AppBar(
+      //   title: Text('Login Page'),
+      // ),
       body: Column(
         children: [
-          Image.asset('assets/images/404_error.png')
+          Image.asset('assets/images/404_error.png'),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Welcome',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 16,
+              horizontal: 32,
+            ),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'username',
+                    hintText: 'enter the usernsme',
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'password',
+                    hintText: 'enter password',
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Login',
+                style: TextStyle(color: Colors.white),
+              )),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
         primaryTextTheme: GoogleFonts.latoTextTheme(),
+        fontFamily: GoogleFonts.lato().fontFamily,
       ),
       routes: {
         "/": (context) => LoginPage(),
